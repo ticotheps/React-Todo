8e867dd4e3ed16cb84be9415a1b05384edcc5421
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-14 18:26:33 -0500

    Created styling for .todo:hover and for input element

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,9 +3,9 @@ import React from 'react';
 const TodoForm = props => {
     return (
         <form className="todo-form">
-	        <input type="text" value={props.task} name="task" onChange={props.handleChanges} />
-	        <button onClick={props.addTodo}>Add a New Todo Item</button>
-	        <button onClick={props.clearCompleted}>Clear Completed Todos</button>
+	        <input className="user-input" type="text" value={props.task} name="task" onChange={props.handleChanges} />
+	        <button className="add-btn" onClick={props.addTodo}>Add a New Todo Item</button>
+	        <button className="clear-btn" onClick={props.clearCompleted}>Clear Completed Todos</button>
         </form>
     );
 };
